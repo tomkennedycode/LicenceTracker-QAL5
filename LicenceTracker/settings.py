02438a162963086b4424c0dd98b6bb616ec97904
Tom Kennedy <tom.kennedy@bp.com>
2022-09-16 16:51:45 +0100

    heroku set up

--- LicenceTracker/settings.py
+++ LicenceTracker/settings.py
@@ -129,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/admin/login'
 LOGIN_REDIRECT_URL = '/admin/login'
+"import django_heroku" 
+"django_heroku.settings(locals())" 
