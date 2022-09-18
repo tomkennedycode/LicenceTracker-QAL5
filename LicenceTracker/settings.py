48fa5f9a6e6e4904b793e356668b548a47be4be0
Tom Kennedy <tom.kennedy@bp.com>
2022-09-18 13:50:58 +0100

    removed secret key

--- LicenceTracker/settings.py
+++ LicenceTracker/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-26b50sjp*ucd$m7wbj7me*tmmqq1novcu&%vd_u+q$oooajz19'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
